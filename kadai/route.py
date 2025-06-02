40f2cf0b0bc07aec35b6b821af6fd51ef73ee44f
mattya333 <c4p31101@bunkyo.ac.jp>
2025-06-03 03:06:15 +0900

    Add files via upload

---
+++ kadai/route.py
@@ -0,0 +1,34 @@
+from flask import request, redirect, send_from_directory
+import controller
+
+def create_route(app):
+
+    @app.route("/")
+    def index():
+        return controller.top()
+    
+    @app.route("/sinsei")
+    def index1():
+        return controller.sinsei()
+
+    @app.route("/toukou")
+    def index2():
+        return controller.toukou()
+    
+    @app.route("/detail")
+    def index3():
+        return controller.detail()
+
+    @app.route("/login")
+    def index4():
+        return controller.login()
+
+
+    @app.route("/json", methods=["GET","POST"])
+    def json():
+        if "data" in request.args:
+            d = request.args.get("data")
+            if d in ["user","item","category","dept"]:
+                return controller.json(d)
+        return("GETパラメータを指定してください")
+    
