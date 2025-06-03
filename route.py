3a48f1fd58668a04f1719123da3463c67d53540f
ENARI-0426 <c4p31019@bunkyo.ac.jp>
2025-06-03 13:05:17 +0900

    Add files via upload

--- route.py
+++ route.py
@@ -1,4 +1,5 @@
-from flask import request, redirect, send_from_directory
+from flask import request, session,redirect, send_from_directory, url_for
+
 import controller
 
 def create_route(app):
@@ -7,13 +8,43 @@ def create_route(app):
     def index():
         return controller.top()
     
-    @app.route("/sinsei")
+    @app.route("/login")
     def index1():
-        return controller.sinsei()
-
-    @app.route("/toukou")
+        username = url_for("loginA")
+        list = [{session.get("user")},{session.get("pass")}]
+        if "user" in session:
+            html += f"""こんにちは{session.get("user")}さん
+        ここは職員ログインページです
+         <form method="POST" action="{username}">ユーザ名を入力してください：<input name="user"></br>"""
+        return html
+    
+    @app.route("/loginA", methods=["POST"])
+    def login():
+        session["user"] = request.form.get("user")
+ 
+        return redirect(url_for("login"))
+    
+    @app.route("/syuutokubutu")
     def index2():
-        return controller.toukou()
+        html = """ここは落とし物登録ページです"""
+        return html
+    
+    @app.route("/serch")
+    def index3():
+        html = """ここは落とし物検索ページです"""
+        return html
+    
+    @app.route("/list")
+    def index4():
+        html = """一覧表示ページです"""
+        return html
+    
+    @app.route("/irai")
+    def index5():
+        html = """依頼ページです"""
+        return html
+    
+
     
     @app.route("/json", methods=["GET","POST"])
     def json():
