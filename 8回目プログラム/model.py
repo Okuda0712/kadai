ae69188cd5208c274e2689429a347264340f1e61
Okuda Akito <ozhangren@gmail.com>
2025-06-06 22:18:48 +0900

    変更の説明メッセージ

--- 8回目プログラム/model.py
+++ 8回目プログラム/model.py
@@ -20,7 +20,6 @@ class ユーザ(db.Model):
     所属ID = db.Column(db.Integer)
     電話番号 = db.Column(db.String(100))
     メールアドレス = db.Column(db.String(100))
-    住所 = db.Column(db.String(100))
 class 所属(db.Model):
     __tablename__ = "所属"
     ID = db.Column(db.Integer, primary_key = True)
@@ -30,17 +29,19 @@ class 拾得物(db.Model):
     ID = db.Column(db.Integer, primary_key = True)
     拾得物分類ID = db.Column(db.Integer)
     拾得場所 = db.Column(db.String(100))
-    色 = db.Column(db.String(50))
     特徴 = db.Column(db.String(100))
-    画像 = db.Column(db.String(100))
+    拾得時間 = db.Column(db.DateTime)
 class 拾得物分類(db.Model):
     __tablename__ = "拾得物分類"
     ID = db.Column(db.Integer, primary_key = True)
-    大分類 = db.Column(db.String(50))
-    物品名 = db.Column(db.String(100))
-    頭1 = db.Column(db.String(50))
-    頭2 = db.Column(db.String(50))
-    五十音 = db.Column(db.String(50))
-    貴重品 = db.Column(db.Boolean)
+    分類 = db.Column(db.String(100))
+class 拾得物管理状況(db.Model):
+    __tablename__ = "拾得物管理状況"
+    ID = db.Column(db.Integer, primary_key = True)
+    ユーザID = db.Column(db.Integer)
+    拾得物ID = db.Column(db.Integer)
+    変更日時 = db.Column(db.DateTime)
+    変更内容 = db.Column(db.String(100))
+    返却日 = db.Column(db.DateTime)
 
 #多分ok
